bf757d65164d5a8e0058eca9c6f0db3eeab48306
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-08-28 15:49:35 +0200

    esercizio completato

---
+++ script.js
@@ -0,0 +1,19 @@
+// let numbers  = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
+
+function numbers(...n) {
+    let organized = n.sort((a, b) => a - b);
+    console.log(organized);
+    let reversed = organized.reverse();
+    console.log(reversed);
+    return;
+}
+
+numbers(3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25);
+
+
+
+                    // Appunto per la Q&A 
+// come fare se volessi stampare in console la frase: "la versione capovolta dell'array è" seguita dall'array
+
+
+
